8ad25dfd345b28c995cfd3fefd11405e974deadf
An Tran <an.t.thien@hiof.no>
2016-11-10 17:27:17 +0100

    Add mongodb-update.js

---
+++ playground/mongodb-update.js
@@ -0,0 +1,33 @@
+﻿const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+    if (err) {
+        return console.log('Error');
+    }
+
+    console.log('Connected to the server');
+
+    //db.collection('Todos').findOneAndUpdate({
+    //    _id: new ObjectID("5824781b07d319ce6ddadd5c")
+    //}, {
+    //        $set: {
+    //            completed: true
+    //        }
+    //    }, {
+    //        returnOriginal: false
+    //}).then((result) => {
+    //        console.log(result);
+    //    });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("58244abe4398df1544e3fdff")
+    }, {
+            $inc: {
+                age: 5
+            }
+        }, {
+            returnOriginal: false
+        }).then((result) => {
+            console.log(result);
+        });
+});
