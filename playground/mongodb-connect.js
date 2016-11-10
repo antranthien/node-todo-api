24bd33a2fbc65139151aa63845e11d783044d75c
An Tran <an.t.thien@hiof.no>
2016-11-10 14:34:20 +0100

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,41 @@
+﻿//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+//MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+//    if (err) {
+//        return console.log('Unable to connect to the server');
+//    }
+
+//    console.log('Connected to the server');
+
+//    //db.collection('Todos').insertOne({
+//    //    text: 'Something to do',
+//    //    completed: false
+//    //}, (err, result) => {
+//    //    if (err) {
+//    //        return console.log('Unable to insert a collection');
+//    //    }
+
+//    //    console.log(JSON.stringify(result.ops, undefined, 2));
+//    //    });
+
+//    // Insert a new doc into the collection Users
+
+//    db.collection('Users').insertOne({
+//        name: 'An Tran',
+//        age: 26,
+//        location: 'Norway'
+//    }, (err, result) => {
+//        if (err) {
+//            return console.log('Unable to insert a document into the Users collection');
+//        }
+
+//        console.log(result.ops[0]._id.getTimestamp());
+//        });
+//    db.close();
+//});
+
---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+﻿const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+    if (err) {
+        return console.log('Error');
+    }
+
+    // find; create a cursor
+    // ToArray: return a promise
+    //db.collection('Todos').find({
+    //    _id: new ObjectID("5824521607d319ce6ddadd59")
+    //}).toArray().then((docs) => { 
+    //    console.log('Todos');
+    //    console.log(JSON.stringify(docs, undefined, 2));
+    //}, (err) => {
+    //    console.log('Unable to fetch documents');
+    //    }); 
+
+    //db.collection('Todos').count().then((count) => {
+    //    console.log(count);
+    //}, (err) => {
+    //    console.log(err);
+    //});
+
+
+    //db.close();
+});
