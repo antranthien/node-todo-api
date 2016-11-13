cc0292b98932f164170f9e0221553b7503f9f575
An Tran <an.t.thien@hiof.no>
2016-11-13 16:39:01 +0100

    Add mongoose-queries.js

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,44 @@
+﻿const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+//var id = '582788c709beee16f86dcec1ss';
+
+//if (!ObjectID.isValid(id)) {
+//    console.log('ObjectID is not valid');
+//}
+//Todo.find({
+//    _id: id
+//}).then((todos) => {
+//    console.log(todos);
+//    });
+
+//Todo.findOne({
+//    _id: id
+//}).then((todo) => {
+//    console.log(todo);
+//    });
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    console.log(todo);
+}).catch((err) => {
+    console.log(err);
+    });
+
+const userId = '5824a6991ee0f137fcd8a575';
+
+User.findById(userId).then((user) => {
+    if (!user) {
+        return console.log(`Cannot find the user with id ${userId}`);
+    }
+
+    console.log(user);
+}).catch((err) => {
+    console.log(err);
+    });
